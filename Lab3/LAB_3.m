615a9de0c26d1ac1a796c0d706c54232d57dcc6e
HDGelement <HDGelement@users.noreply.github.com>
2019-10-08 10:22:00 -0400

    Fixed some problems
    - All graphs now show up in different figures
    - New comments
    - you actually needed to plot a matrix not just the whole thing .

--- Lab3/LAB_3.m
+++ Lab3/LAB_3.m
@@ -1,21 +1,24 @@
 %%part 1
 f0 = 200;
 t = 0:0.0001:10;
-y = cos(2*pi*f0*t); 
+y = cos(2*pi*f0*t);
 
 %a
 SpectrumAnalyzer(y, 10000);
-figure(1)
-plot(t, y, '.');
+figure(2)
+plot(t(1: 500), y(1:500), 'b-'); %% ty
 soundsc(y, 10000);
 hold on;
 
 %b
 f0 = 400;
-plot(t, y, 'r--');
-xlabel('Time');
-ylabel('Power');
-title('Plotted Wave Graph')
-SpectrumAnalyzer(y, 10000);
+y1 = cos(2*pi*f0*t);
+figure(3)
+plot(t(1:500), y1(1:500), 'r-');
+SpectrumAnalyzer(y1, 10000);
 soundsc(y, 10000);
 
+%%part 2, y1 and y has been used
+k = 3;
+y2 = sin((k * pi / 2) / k)*cos(k * 2 * pi * f0 * t);
+%%plot(t, y1, 'bl');
