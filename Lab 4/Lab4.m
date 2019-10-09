3d6103aba714367d0a715ec6c13ae66377a78b04
HDGelement <HDGelement@users.noreply.github.com>
2019-10-08 21:25:19 -0400

    Started Lab 4 + 1st Finish on lab 3
    - did D.) for lab 3
    - added scary sound
    - used FilterDesigner (type in cmd or command prompt)
    - end my suffering

---
+++ Lab 4/Lab4.m
@@ -0,0 +1,20 @@
+%%part 1
+Points = input ('Grade: ');
+E = 0-300
+D = 300-320
+D = 320-340
+D =  340-360
+C = 360-380
+C = 380-400
+C+ 400-420
+B- 420-450
+B 450-480
+B+ 480-510
+A- 510-540
+A 540-570
+A+ 570-600
+function Finalgrade = Grade(Points)
+    if(Points => )
+end
+
+    
--- Lab3/LAB_3.m
+++ Lab3/LAB_3.m
@@ -31,14 +31,14 @@ plot(t(1:(length(t))/200), y2(1:(length(y2))/200), 'g--');
 
 for i = 1:10
     k = 1 + (i*2);
-    fprintf("this is my number %0f \n" ,k);
-    y2 = y + sin((k * pi / 2) / k)*cos(k * 2 * pi * f0 * t);
+    fprintf("this is my number %0f \n" ,k); %debugging
+    y2 = y + sin((k * pi / 2) / k)*cos(k * 2 * pi * f0 * t); % you dont need
     figure(k+2);
     plot(t(1:(length(t))/200), y2(1:(length(y2))/200), 'g-');
     SpectrumAnalyzer(y2, 10000);
-    movegui(figure(3 + (i*2)), 'east')
-    movegui(figure(4 + (i*2)), 'west')
-    fprintf("move me %0f \n", (((i-1)*2)+5));
+    movegui(figure(3 + (i*2)), 'east') % you dont need
+    movegui(figure(4 + (i*2)), 'west') % you dont need
+    fprintf("move me %0f \n", (((i-1)*2)+5)); % you dont need 
     soundsc(y2, 10000)
     %pause(5);
 end
@@ -50,6 +50,11 @@ y3=filter(Num,1,y2); %% y2 = squarewave
 plot(t(1:500), y3(1:500), 'r--');
 soundsc(y3, 10000)
 
+%%part 4 yes my guy, END MY SUFFERING
 SpectrumAnalyzer(y3, 10000)
-soundsc(y3, 12000);
+soundsc(y3, 12000); % im scared shitless
+y3 = filter(Num,1,y3);
+plot(t(1:500), y3(1:500));
+SpectrumAnalyzer(y3, 12000);
 %you suck
+%DONE 
